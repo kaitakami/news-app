4e1a761508ee927e9e745e5d2f77d7ca60d7bd43
Kai Takami <kaitakami@protonmail.com>
2022-10-17 17:51:55 +0900

    Create about page

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PageLayout from "../components/PageLayout";
+
+const About = () => {
+  return (
+    <PageLayout title="About">
+      <div>about</div>
+    </PageLayout>
+  );
+};
+
+export default About;
